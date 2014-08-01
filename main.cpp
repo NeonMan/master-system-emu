238439655cbe06c1439537ad3ad9e19225577f5f
J.Luis <root@heavydeck.net>
2014-08-01 04:52:39 +0200

    Added a main, which generates a raw s16b_be audio file.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include "psg.h"
 
 using namespace std;
+//Generate a couple seconds of sound
 int main(int argc, char *argv[]) {
-
+    unsigned long sample_count = 0;
+    unsigned long cycle_count = 0;
+    while (sample_count < (44100 * 10)){
+        ++cycle_count;
+        if (psg::tick()){
+            ++sample_count;
+            putchar(*((char*)&(psg::state::next_sample) + 1));
+            putchar(*(char*)&(psg::state::next_sample));
+        }
+    }
 }
--- psg.cpp
+++ psg.cpp
@@ -11,13 +11,13 @@ namespace psg{
         uint_fast32_t sample_rate = 22050;            ///<-- Output sample rate, in Hz
         const uint32_t psg_clock_ntsc = 3579545 / 16; ///<-- PSG clock (pal NTSC version)
         const uint32_t psg_clock_pal  = 3546893 / 16; ///<-- PSG clock (pal PAL version)
-        uint_fast32_t psg_clock = psg_clock_pal;      ///<-- PSG clock
+        uint_fast32_t psg_clock = psg_clock_ntsc;     ///<-- PSG clock
     }
     
     ///PSG emulator state
     namespace state{
         uint_fast32_t cycle = 0; ///<-- Current clock cycle
-        int16_t next_sample = 128; ///<-- Generated sample
+        int16_t next_sample = 0; ///<-- Generated sample
         ///Clock ratio between psg clock and sample rate *128
         uint_fast32_t clock_ratio = (cfg::psg_clock << 7) / cfg::sample_rate;
         ///Increased by 128 every clock cycle, if greater than clock_ratio,
@@ -32,43 +32,43 @@ namespace psg{
 
     ///PSG writeable registers
     namespace reg{
-        uint_fast8_t  vol[4] = { 15, 15, 15, 15 }; //Full volume.
-        uint_fast16_t tone[4] = { 0xFE, 0xFE, 0xFE, 0xFE }; //440 Hz
+        int_fast8_t  vol[4] = { 0, 15, 15, 15 }; //Full volume.
+        uint_fast16_t tone[4] = { 0xFE, 0xBE, 0xDE, 0xFE }; //440 Hz
     }
 
-    ///Generate a sample from the registers
-    inline int16_t make_sample(){
-        const int16_t sample = (
-            //state::channel_polarity[0] ? reg::vol[0] : -reg::vol[0] +
-            state::channel_polarity[1] ? reg::vol[1] : -reg::vol[1] +
-            state::channel_polarity[2] ? reg::vol[2] : -reg::vol[2] +
-            state::channel_polarity[3] ? reg::vol[3] : -reg::vol[3]
-            );
-        return sample;
+    ///Generate a sample from the registers (Signed 16bit)
+    int16_t make_sample(){
+        int16_t sample = 0;
+        sample += state::channel_polarity[0] ? reg::vol[0] : -reg::vol[0];
+        sample += psg::state::channel_polarity[1] ? psg::reg::vol[1] : -psg::reg::vol[1];
+        sample += psg::state::channel_polarity[2] ? psg::reg::vol[2] : -psg::reg::vol[2];
+        sample += psg::state::channel_polarity[3] ? psg::reg::vol[3] : -psg::reg::vol[3];
+        return sample*128; //4bit volume would be too low, we multiply to make it audible
     }
+
     
     ///Add a clock cycle, return true if there's a new sample ready.
     bool tick(){
-        //Update tone counters
+        //Update tone counters #[1-3]
         for (unsigned int i = 1; i < 4; i++){
-            --state::tone_counter[i];
-            if (state::tone_counter[i] == 0){
-                state::channel_polarity[i] ^= 1;
-                state::tone_counter[i] = reg::tone[i];
+            --psg::state::tone_counter[i];
+            if (psg::state::tone_counter[i] == 0){
+                psg::state::channel_polarity[i] = psg::state::channel_polarity[i] ?  0 : 1;
+                psg::state::tone_counter[i] = psg::reg::tone[i];
             }
         }
 
-        //Update the noise channel
+        //Update the noise channel (#0)
         //ToDo
 
         //Update clock state
-        ++state::cycle;
-        state::clock_current_ratio += 128;
+        ++psg::state::cycle;
+        psg::state::clock_current_ratio += 128;
 
         //Generate sample, if needed
-        if (state::clock_current_ratio >= state::clock_ratio){
-            state::clock_current_ratio = (state::clock_current_ratio & 127) + (state::clock_ratio & 127);
-            state::next_sample = make_sample();
+        if (psg::state::clock_current_ratio >= psg::state::clock_ratio){
+            psg::state::clock_current_ratio = (psg::state::clock_current_ratio & 127) + (psg::state::clock_ratio & 127);
+            psg::state::next_sample = make_sample();
             return true;
         }
         return false;
