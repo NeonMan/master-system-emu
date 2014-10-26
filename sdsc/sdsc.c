b64c5065405ac91db41ea23f34f56aa9c563ebb1
J.Luis <root@heavydeck.net>
2014-10-26 03:29:38 +0100

    Added some skeleton code to the SDSC console control.

--- sdsc/sdsc.c
+++ sdsc/sdsc.c
@@ -5,10 +5,34 @@
 /**
  * Implements the SDSC debug console.
  */
+#define SDSC_CTL_BUFFER_SIZE 10
+uint8_t sdsc_cbuff[SDSC_CTL_BUFFER_SIZE];
+uint8_t sdsc_cbuff_index = 0;
 
 //sdsc console control port 0xFC
 void sdsc_control(uint8_t b){
-    fprintf(stderr, "SDSC control: 0x%X\n", b);
+    if (sdsc_cbuff_index == 0){ //New command! No previous state exists.
+        switch (b){
+        case 1: /* -- Suspend emulation*/
+            fprintf(stderr, "SDSC: Suspend emulation... [!]\n");
+            break;
+        case 2: /* -- Clear console emulation*/
+            fprintf(stderr, "SDSC: Clear screen.\n\f");
+            break;
+        case 3: /* -- Set attribute*/
+            fprintf(stderr, "SDSC: Set attribute... [!]\n");
+            break;
+        case 4: /* -- Move cursor*/
+            fprintf(stderr, "SDSC: Move cursor... [!]\n");
+            break;
+        default:
+            fprintf(stderr, "SDSC: Unknown byte (0x%X)\n", b);
+            break;
+        }
+    }
+    else{ //Multi-byte commands
+
+    }
 }
 
 ///sdsc console data port 0xFD
