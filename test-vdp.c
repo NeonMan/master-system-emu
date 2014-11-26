2d9784241edff1f272f2e2678e75a7894cfd4bdd
J.Luis <root@heavydeck.net>
2014-11-26 22:53:12 +0100

    Completed VDP default pallette.

--- test-vdp.c
+++ test-vdp.c
@@ -1,6 +1,7 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_version.h>
 #include <stdio.h>
+#include "vdp/vdp_constants.h"
 
 int main(int argc, char** argv){
     SDL_version ver;
@@ -15,11 +16,17 @@ int main(int argc, char** argv){
 
     SDL_Window* window = SDL_CreateWindow("VDP testing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256 + 64, 192 + 64, 0);
     SDL_Surface* screen = SDL_GetWindowSurface(window);
-    SDL_Rect video_area; video_area.x = 32; video_area.y = 32; video_area.w = 256; video_area.h = 192;
-    uint32_t bkg = (128<<8) + 255;
-    uint32_t fg = 255;
-    SDL_FillRect(screen, 0, bkg);
-    SDL_FillRect(screen, &video_area, fg);
+    SDL_FillRect(screen, 0, 128);
+    for (int x = 0; x < 4; x++){
+        for (int y = 0; y < 4; y++){
+            SDL_Rect video_area; 
+            video_area.x = 32 + (256 / 4) * x;
+            video_area.y = 32 + (192 / 4) * y;
+            video_area.w = 256/4; 
+            video_area.h = 192/4;
+            SDL_FillRect(screen, &video_area, vdp_tmscolors[(4*y) + x] );
+        }
+    }
     SDL_UpdateWindowSurface(window);
     SDL_Delay(5000);
     SDL_Quit();
