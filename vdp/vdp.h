3f1e5ea8ab8ecc9aab87313f7b8ea175baff79ef
J.Luis <root@heavydeck.net>
2014-11-27 18:07:29 +0100

    Implemented Mode0 rendering (Untested). Moved 'magic' numbers to #define macros.

--- vdp/vdp.h
+++ vdp/vdp.h
@@ -3,6 +3,7 @@
 
 //VDP requires the Address bus, data bus and the #INT pin
 #include "../z80/z80_externs.h"
+#include "vdp_constants.h"
 #include <stdint.h>
 
 //Values for the upper 2-bits from a control word (bits 14,15).
@@ -19,7 +20,7 @@
 #define VDP_REG_MODE_CTRL2 1
 #define VDP_REG_NAME_TABLE_ADDR 2
 #define VDP_REG_COLOR_TABLE_ADDR 3
-#define VDP_REG_BKG_PATTERN_ADDR 4
+#define VDP_REG_PATTERN_GENERATOR_ADDR 4
 #define VDP_REG_SPR_ATTRIBUTE_ADDR 5
 #define VDP_REG_SPR_PATTERN_ADDR 6
 #define VDP_REG_TEXT_COLOR 7
@@ -45,7 +46,7 @@
 #define VDP_FLAG_MAG (vdp.regs[1] & (1<<0))
 
 #ifdef __cplusplus
-//extern "C" {
+extern "C" {
 #endif
 
 //Types
@@ -54,8 +55,8 @@ struct vdp_s {
     uint8_t ioreq_done;
 
     //Ram, registers and stuff
-    uint8_t cram[32]; ///<-- Color RAM, 32 bytes
-    uint8_t vram[16 * 1024]; ///<-- VRAM, 16K
+    uint8_t cram[VDP_CRAM_SIZE]; ///<-- Color RAM, 32 bytes
+    uint8_t vram[VDP_VRAM_SIZE]; ///<-- VRAM, 16K
     //uint8_t v; ///<-- V counter (Register #10)
     uint8_t h; ///<-- H counter
 	uint8_t buffer;
@@ -68,7 +69,7 @@ struct vdp_s {
 	uint8_t control_index; ///<-- Which control word byte are we expecting [0,1]
 	uint8_t control_mode;
 
-    uint8_t framebuffer[256 * 192]; ///<-- Current picture being displayed
+    uint8_t framebuffer[VDP_FRAMEBUFFER_SIZE]; ///<-- Current picture being displayed
 };
 
 ///executes the minimum relevant period of time.
@@ -81,7 +82,7 @@ void vdp_init();
 void* vdp_get_pixels();
 
 #ifdef __cplusplus
-//}
+}
 #endif
 
 #endif
