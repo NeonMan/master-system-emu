2e81b88328f582baef7c261d5cfcc5590082ad3f
J.Luis <root@heavydeck.net>
2014-11-29 03:58:32 +0100

    Added skeleton to undocumented Mode1+2/1+3/2+3/1+2+3.

--- vdp/vdp_render.h
+++ vdp/vdp_render.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// --- TMS modes ---
 ///Renders picture to framebuffer assuming Mode0
 void vdp_mode0_pixels(uint8_t* fb);
 ///Renders picture to framebuffer assuming Mode1 (TEXT)
@@ -11,7 +12,19 @@ void vdp_mode1_pixels(uint8_t* fb);
 void vdp_mode2_pixels(uint8_t* fb);
 ///Renders picture to framebuffer assuming Mode3 (MULTICOLOR)
 void vdp_mode3_pixels(uint8_t* fb);
+
+// --- SMS Mode 4 ---
 ///Renders picture to framebuffer assuming Mode 4 (SMS)
 void vdp_mode4_pixels(uint8_t* fb);
+
+// --- Undocumented modes ---
+///Renders picture to framebuffer assuming mode 1+2
+void vdp_mode12_pixels(uint8_t* fb);
+///Renders picture to framebuffer assuming mode 1+3
+void vdp_mode13_pixels(uint8_t* fb);
+///Renders picture to framebuffer assuming mode 2+3
+void vdp_mode23_pixels(uint8_t* fb);
+///Renders picture to framebuffer assuming mode 1+2+3
+void vdp_mode123_pixels(uint8_t* fb);
 
 #endif
