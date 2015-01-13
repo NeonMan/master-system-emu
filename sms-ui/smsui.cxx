2abe51bd88766dc2c535953228e45d99b57902c4
J.Luis <root@heavydeck.net>
2015-01-13 13:12:47 +0100

    Z80 dialog reads its information from the actual z80 struct.

--- sms-ui/smsui.cxx
+++ sms-ui/smsui.cxx
@@ -20,6 +20,18 @@
 #include <z80/z80.h>
 #include "sms-emu.h" /*<-- Refactor me!*/
 
+// --- Helper functions & macros ---
+
+//Update UI, where X is a milisecond delta since last update
+#define __UPDATE_FLTK \
+{ \
+    const DWORD ticks = GetTickCount(); \
+    if ((ticks % 10) == 0) Fl::check();  /*Refresh FLTK every .1 seconds*/ \
+    if ((ticks % 100) == 0) dlg_z80->update_values(); /*Refresh Values every second*/ \
+    if (!dlg_z80->windowDialog->shown()) \
+        is_running = 0; \
+}
+
 void emu_log(const char* msg, int level){
     std::cerr << "[" << emu_logelvel_names[level][0] << "] " << msg << std::endl;
 }
@@ -29,58 +41,79 @@ void emu_init(){
 }
 
 void emu_cleanup(){
+    
     //SDL_Quit();
     emu_log("Bye!", EMU_LOG_INFO);
 }
 
+// --- Emulator ---
+void emu_loop(){
+
+}
 
 int main(int argc, char** argv){
     emu_init();
     //Create dialogs
-    DialogZ80* z80         = new DialogZ80;
-    DialogBreakpoints* brk = new DialogBreakpoints;
+    DialogZ80* dlg_z80         = new DialogZ80;
+    DialogBreakpoints* dlg_brk = new DialogBreakpoints;
 
     //Show dialogs
-    z80->windowDialog->show();
+    
+    dlg_z80->windowDialog->show();
     //brk->windowDialog->show();
 
     volatile uint8_t is_running = 1; //<-- When this becomes false, the app exits
     volatile uint8_t is_clocked = 1; //<-- When this becames false, the execution is paused.
 
     //Provide the UI with relevant variables
-    struct z80_s fake_z80;
-    for (int i = 0; i < sizeof(struct z80_s); i++){
-        *(((uint8_t*)&fake_z80) + i) = i & 0xFF;
-    }
+    dlg_z80->set_running_ptr((uint8_t*) &is_clocked);
+    dlg_z80->set_z80_ptr(z80dbg_get_z80());
 
-    z80->set_running_ptr((uint8_t*) &is_clocked);
-    z80->set_z80_ptr(&fake_z80);
-
-    int tick_count = 0;
+    int edge_count = 0;
     while (is_running){
         while (is_clocked && is_running){
-            ++tick_count;
-            if ((tick_count%20) == 0) std::cout << "Tick!" << std::endl;
-
-            //Update UI
-            z80->update_values();
-            Fl::check();
-            Fl::wait(0.1);
-            if (!z80->windowDialog->shown())
-                is_running = 0;
+            //
+            // Every clock edge, the z80 is 'ticked'. Other modules must be clocked
+            // synchronously at different rates (clock divider). Other modules can
+            // be ticked asynchronosly when required.
+            //
+            // Synchronous modules:
+            //   * z80
+            //   * PSG (16 prescaler/32 edge prescaler)
+            //   * VDP (no prescaler?)
+            //
+            // Asynchronous modules:
+            //  * RAM
+            //  * ROM
+            //  * IO  (must be ticked before any MREQ/IOREQ operation)
+            //  * Peripherial
+            //
+            z80_tick();
+            if ((edge_count % 2) == 0)
+                vdp_tick();
+            if ((edge_count % 32) == 0)
+                psg_tick();
+            if ((!z80_n_mreq) && ((!z80_n_rd) || (!z80_n_wr))){ //Memory-mapped operation
+                io_tick();
+                rom_tick();
+                ram_tick();
+            }
+            if ((!z80_n_ioreq) && ((!z80_n_rd) || (!z80_n_wr))){ //IO operation
+                io_tick();
+                per_tick();
+                psg_io();
+            }
+            __UPDATE_FLTK;
+            ++edge_count;
         }
         // --- Just update the UI if not clocked ---
         {
-            //Update UI
-            Fl::check();
-            Fl::wait(0.1);
-            if (!z80->windowDialog->shown())
-                is_running = 0;
+            __UPDATE_FLTK;
         }
     }
     //Cleanup
-    delete z80;
-    delete brk;
+    delete dlg_z80;
+    delete dlg_brk;
     emu_cleanup();
     return 0;
 }
