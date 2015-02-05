b23b0f9dbedae2c0b5e4f190d703b25d587bb80b
J.Luis <root@heavydeck.net>
2015-02-05 13:19:52 +0100

    Refactored some opcodes out of the decoder.
    * CCF
    * CPL
    * DEC r
    * LD r, (HL)
    * LD r, r
    * RLA
    * RLCA
    * RRA
    * RRCA
    * SCF

--- z80/z80_opcodes.c
+++ z80/z80_opcodes.c
@@ -1,6 +1,7 @@
 #include "z80_opcodes.h"
 #include "z80_macros.h"
 #include "z80_register_lut.h"
+#include <assert.h>
 
 extern struct z80_s z80; //<-- Access to z80 internals
 
@@ -26,6 +27,36 @@ int z80_op_ADD_n(){
     Z80_F |= Z80_SETFLAG_HC(orig_a, Z80_A);
     Z80_F |= Z80_SETFLAG_OVERFLOW(orig_a, Z80_A);
     Z80_F |= Z80_SETFLAG_CARRY(orig_a, Z80_A);
+    return Z80_STAGE_RESET;
+}
+
+///CCF; Size: 1; Flags: C
+int z80_op_CCF(){
+    Z80_F = (Z80_F ^ Z80_FLAG_CARRY) & Z80_CLRFLAG_ADD;
+    return Z80_STAGE_RESET;
+}
+
+///CPL; Size: 1; Flags: H,N
+int z80_op_CPL(){
+    Z80_A = ~Z80_A;
+    Z80_F = Z80_F | Z80_FLAG_HC | Z80_FLAG_ADD;
+    return Z80_STAGE_RESET;
+}
+
+///DEC(r[y]); Size:1; Flags: S,Z,H,P,N
+///@bug will crash on (HL)
+int z80_op_DEC_r(){
+    Z80_OPCODE_SUBDIV;
+    const uint8_t old_r = *z80_r[y[0]];
+    --(*(z80_r[y[0]]));
+    Z80_F = (Z80_F & (
+        Z80_CLRFLAG_SIGN & Z80_CLRFLAG_ZERO & Z80_CLRFLAG_HC
+        & Z80_CLRFLAG_PARITY & Z80_CLRFLAG_ADD)
+        )  //Clear S,Z,H,P,N (7,6,4,2,1) ***V0-
+        | Z80_SETFLAG_SIGN(*z80_r[y[0]])
+        | Z80_SETFLAG_ZERO(*z80_r[y[0]])
+        | Z80_SETFLAG_HC(old_r, *z80_r[y[0]])
+        | Z80_SETFLAG_OVERFLOW(*z80_r[y[0]], old_r);
     return Z80_STAGE_RESET;
 }
 
@@ -131,8 +162,83 @@ int z80_op_LD_DEp_A(){
     }
 }
 
+///LD r[y], (HL); Size:1; Flags: None
+int z80_op_LD_r_HLp(){
+    //Source is always (HL, Target is never (HL)
+    Z80_OPCODE_SUBDIV;
+    if (z80.read_index == 0){
+        z80.read_address = Z80_HL;
+        return Z80_STAGE_M2;
+    }
+    else{
+        *(z80_r[y[0]]) = z80.read_buffer[0];
+        return Z80_STAGE_RESET;
+    }
+}
+
+///LD r[y],r[z]; Size: 1; Flags: None
+int z80_op_LD_r_r(){
+    //Source can never be (HL). That implies z[0]==6
+    //Target can be (HL)
+    Z80_OPCODE_SUBDIV;
+    if (z80_r[y[0]]){ //If target != (HL)
+        *(z80_r[y[0]]) = *(z80_r[z[0]]);
+        return Z80_STAGE_RESET;
+    }
+    else{ //Target is (HL)
+        if (z80.write_index == 0){
+            z80.write_address = Z80_HL;
+            assert(z80_r[z[0]]);
+            z80.write_buffer[0] = *(z80_r[z[0]]);
+            return Z80_STAGE_M3;
+        }
+        else{
+            return Z80_STAGE_RESET;
+        }
+    }
+}
+
 ///NOP; Size: 1; Flags: None
 int z80_op_NOP(){
+    return Z80_STAGE_RESET;
+}
+
+int z80_op_RLA(){
+    const uint8_t next_carry = Z80_A & (1 << 7);
+    Z80_A = (Z80_A << 1) | (Z80_F & Z80_FLAG_CARRY ? 1 : 0);
+    Z80_F = (Z80_F & (Z80_CLRFLAG_HC & Z80_CLRFLAG_ADD & Z80_CLRFLAG_CARRY))
+        | (next_carry ? Z80_FLAG_CARRY : 0);
+    return Z80_STAGE_RESET;
+}
+
+///RLCA; Size: 1; Flags: H,N,C
+int z80_op_RLCA(){
+    Z80_A = (Z80_A << 1) | (Z80_A & (1 << 7) ? 1 : 0);
+    Z80_F = (Z80_F & (Z80_CLRFLAG_HC & Z80_CLRFLAG_ADD & Z80_CLRFLAG_CARRY))
+        | ((Z80_F & (1)) ? Z80_FLAG_CARRY : 0);
+    return Z80_STAGE_RESET;
+}
+
+///RRA; Size: 1; Flags: H,N,C
+int z80_op_RRA(){
+    const uint8_t next_carry = Z80_A & (1);
+    Z80_A = (Z80_A >> 1) | (Z80_F & Z80_FLAG_CARRY ? (1 << 7) : 0);
+    Z80_F = (Z80_F & (Z80_CLRFLAG_HC & Z80_CLRFLAG_ADD & Z80_CLRFLAG_CARRY))
+        | (next_carry ? Z80_FLAG_CARRY : 0);
+    return Z80_STAGE_RESET;
+}
+
+///RRCA; Size: 1; Flags: H,N,C
+int z80_op_RRCA(){
+    Z80_A = (Z80_A >> 1) | (Z80_A & 1 ? (1 << 7) : 0);
+    Z80_F = (Z80_F & (Z80_CLRFLAG_HC & Z80_CLRFLAG_ADD & Z80_CLRFLAG_CARRY))
+        | ((Z80_F & (1 << 7)) ? Z80_FLAG_CARRY : 0);
+    return Z80_STAGE_RESET;
+}
+
+///SCF; Size: 1; Flags: C
+int z80_op_SCF(){
+    Z80_F = (Z80_F & (Z80_CLRFLAG_HC & Z80_CLRFLAG_ADD)) | Z80_FLAG_CARRY;
     return Z80_STAGE_RESET;
 }
 
--- z80/z80_opcodes.h
+++ z80/z80_opcodes.h
@@ -5,6 +5,9 @@
 
 int z80_op_ADD_HL_rp();
 int z80_op_ADD_n();
+int z80_op_CCF();
+int z80_op_CPL();
+int z80_op_DEC_r();
 int z80_op_DEC_rp();
 int z80_op_EX();
 int z80_op_INC_HLp();
@@ -13,6 +16,13 @@ int z80_op_INC_rp();
 int z80_op_LD_A_DEp();
 int z80_op_LD_BCp_A();
 int z80_op_LD_DEp_A();
+int z80_op_LD_r_HLp();
+int z80_op_LD_r_r();
 int z80_op_NOP();
+int z80_op_RLA();
+int z80_op_RLCA();
+int z80_op_RRA();
+int z80_op_RRCA();
+int z80_op_SCF();
 
 #endif
