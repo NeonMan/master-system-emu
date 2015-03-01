87129a54e6e0060c477039762c46bdd4343cf443
J.Luis <root@heavydeck.net>
2015-03-01 02:01:48 +0100

    Minor changes to test files.

--- test/test_z80/test_z80_decoder.c
+++ test/test_z80/test_z80_decoder.c
@@ -1,3 +1,27 @@
+// Copyright 2015 Juan Luis Álvarez Martínez
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ * This unit test checks the execution correctness of opcodes,
+ * in essence it will check the end stage of decode/execution.
+ *
+ * Return value of the decoder call *must* be Z80_STAGE_RESET and
+ * any side effects leading to this last call will be ignored or
+ * mocked. This test will be unable to test the correctness of
+ * neither main memory access nor any other M1/M2/M3 stage actions.
+ */
+
 #include "unity.h"
 #include "unity_fixture.h"
 #include "z80/z80.h"
@@ -6,13 +30,13 @@
 #include <string.h>
 
 //Test macros
-#define TEST_AF(E,F)  TEST_ASSERT_EQUAL(E.rAF[0],F.rAF[0]); TEST_ASSERT_EQUAL(E.rAF[1],F.rAF[1]);
+#define TEST_AF(E,F)  TEST_ASSERT_EQUAL(E.rAF[0],F.rAF[0]);  TEST_ASSERT_EQUAL(E.rAF[1],F.rAF[1]);
 #define TEST_AFp(E,F) TEST_ASSERT_EQUAL(E.rAF[2], F.rAF[2]); TEST_ASSERT_EQUAL(E.rAF[3], F.rAF[3]);
-#define TEST_BC(E,F)  TEST_ASSERT_EQUAL(E.rBC[0],F.rBC[0]); TEST_ASSERT_EQUAL(E.rBC[1],F.rBC[1]);
+#define TEST_BC(E,F)  TEST_ASSERT_EQUAL(E.rBC[0],F.rBC[0]);  TEST_ASSERT_EQUAL(E.rBC[1],F.rBC[1]);
 #define TEST_BCp(E,F) TEST_ASSERT_EQUAL(E.rBC[2], F.rBC[2]); TEST_ASSERT_EQUAL(E.rBC[3], F.rBC[3]);
-#define TEST_DE(E,F)  TEST_ASSERT_EQUAL(E.rDE[0],F.rDE[0]); TEST_ASSERT_EQUAL(E.rDE[1],F.rDE[1]);
+#define TEST_DE(E,F)  TEST_ASSERT_EQUAL(E.rDE[0],F.rDE[0]);  TEST_ASSERT_EQUAL(E.rDE[1],F.rDE[1]);
 #define TEST_DEp(E,F) TEST_ASSERT_EQUAL(E.rDE[2], F.rDE[2]); TEST_ASSERT_EQUAL(E.rDE[3], F.rDE[3]);
-#define TEST_HL(E,F)  TEST_ASSERT_EQUAL(E.rHL[0],F.rHL[0]); TEST_ASSERT_EQUAL(E.rHL[1],F.rHL[1]);
+#define TEST_HL(E,F)  TEST_ASSERT_EQUAL(E.rHL[0],F.rHL[0]);  TEST_ASSERT_EQUAL(E.rHL[1],F.rHL[1]);
 #define TEST_HLp(E,F) TEST_ASSERT_EQUAL(E.rHL[2], F.rHL[2]); TEST_ASSERT_EQUAL(E.rHL[3], F.rHL[3]);
 
 #define TEST_PC(E,F)  TEST_ASSERT_EQUAL(E.rPC,F.rPC);
@@ -67,7 +91,7 @@ TEST(unprefixed, opcode_NOP){
     z80.opcode[0] = 0x00;
     z80.opcode_index = 1;
     z80_expected = z80;
-    z80_instruction_decode();
+    TEST_ASSERT_EQUAL(Z80_STAGE_RESET, z80_instruction_decode());
     TEST_REGS(z80_expected, z80);
 }
 
