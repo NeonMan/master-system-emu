cfcdfb6b900c3595a979a04c8e24a9e949400e8e
J.Luis <root@heavydeck.net>
2015-05-23 23:06:43 +0200

    Grouped opcodes by function in header file.

--- z80/z80_opcodes.h
+++ z80/z80_opcodes.h
@@ -17,68 +17,9 @@
 
 #include "z80.h"
 
-/* -------------------------------------------- */
-/* --- Unprefixed and 0xCB prefixed opcodes --- */
-/* -------------------------------------------- */
-
-int ADC_HL_rp();
-int ADC_n();
-int ADC_r();
-int ADC_HLp();
-int ADD_HL_rp();
-int ADD_n();
-int ADD_r();
-int ADD_HLp();
-int AND_HL_rp();
-int AND_n();
-int AND_r();
-int AND_HLp();
-int BIT_b_r();
-int BIT_b_HLp();
-int CALL_cc_nn();
-int CALL_nn();
-int CCF();
-int CPD();
-int CPDR();
-int CPI();
-int CPIR();
-int CPL();
-int CP_n();
-int CP_r();
-int CP_HLp();
-int CP_HL_rp();
-int DAA();
-int DEC_r();
-int DEC_HLp();
-int DEC_rp();
-int DI();
-int DJNZ_d();
-int EI();
-int EX();
-int EXX();
-int EX_DE_HL();
-int EX_SPp_HL();
-int HALT();
-int IM();
-int INC_HLp();
-int INC_r();
-int INC_rp();
-int IND();
-int INDR();
-int INI();
-int INIR();
-int IN_A_np();
-int IN_Cp();
-int IN_r_Cp(); 
-int JP_cc_nn();
-int JP_HLp();
-int JP_nn();
-int JR_cc_d();
-int JR_d();
-int LDD();
-int LDDR();
-int LDI();
-int LDIR();
+/* ------------------ */
+/* --- 8-bit Load --- */
+/* ------------------ */
 int LD_A_BCp();
 int LD_A_DEp();
 int LD_A_I();
@@ -88,59 +29,114 @@ int LD_BCp_A();
 int LD_DEp_A();
 int LD_HL_nnp();
 int LD_HLp_n();
+int LD_HLp_r();
 int LD_I_A();
 int LD_nnp_A();
+int LD_R_A();
+int LD_r_HLp();
+int LD_r_n();
+int LD_r_r();
+
+/* ------------------- */
+/* --- 16-bit Load --- */
+/* ------------------- */
 int LD_nnp_HL();
 int LD_nnp_rp();
 int LD_rp_nn();
 int LD_rp_nnp();
-int LD_R_A();
-int LD_r_n();
-int LD_r_r();
-int LD_r_HLp();
-int LD_HLp_r();
 int LD_SP_HL();
-int NEG();
-int NOP();
+int POP_rp2();
+int PUSH_rp2();
+
+/* ------------------------------------------- */
+/* --- Exchange, block transfer and search --- */
+/* ------------------------------------------- */
+int CPD();
+int CPDR();
+int CPI();
+int CPIR();
+int EX();
+int EX_DE_HL();
+int EX_SPp_HL();
+int EXX();
+int LDD();
+int LDDR();
+int LDI();
+int LDIR();
+
+/* ------------------------ */
+/* --- 8-bit arithmetic --- */
+/* ------------------------ */
+int ADC_HLp();
+int ADC_n();
+int ADC_r();
+int ADD_HLp();
+int ADD_n();
+int ADD_r();
+int AND_HLp();
+int AND_n();
+int AND_r();
+int CP_HLp();
+int CP_n();
+int CP_r();
+int DEC_HLp();
+int DEC_r();
+int INC_HLp();
+int INC_r();
 int OR_HLp();
 int OR_n();
 int OR_r();
-int OTDR();
-int OTIR();
-int OUTD();
-int OUTI();
-int OUT_Cp_0();
-int OUT_Cp_r();
-int OUT_np_A();
-int POP_rp2();
-int PUSH_rp2();
-int RES_b_r();
-int RES_b_HLp();
-int RET();
-int RETI();
-int RETN();
-int RET_cc();
-int RLA();
-int RLCA();
-int RLC_r();
-int RLD();
-int RL_HLp();
-int RL_r();
-int RRA();
-int RRCA();
-int RRC_HLp();
-int RRC_r();
-int RRD();
-int RR_HLp();
-int RR_r();
-int RST_y();
-int SBC_HL_rp(); /*Can't be prefixed*/
 int SBC_HLp();
 int SBC_n();
 int SBC_r();
+int SUB_HLp();
+int SUB_n();
+int SUB_r();
+int XOR_HLp();
+int XOR_n();
+int XOR_r();
+
+/* ---------------------------------------------- */
+/* --- General purpose arithmetic and control --- */
+/* ---------------------------------------------- */
+int CCF();
+int CPL();
+int DAA();
+int DI();
+int EI();
+int HALT();
+int IM();
+int NEG();
+int NOP();
 int SCF();
-int SET_b_r();
-int SET_b_HLp();
+
+/* ------------------------- */
+/* --- 16-bit Arithmetic --- */
+/* ------------------------- */
+int ADC_HL_rp();
+int ADD_HL_rp();
+int AND_HL_rp();
+int CP_HL_rp();
+int DEC_rp();
+int INC_rp();
+int SBC_HL_rp();
+
+/* ------------------------ */
+/* --- Rotate and shift --- */
+/* ------------------------ */
+int RL_HLp();
+int RL_r();
+int RLA();
+int RLC_r();     /**<-- @bug NOT in decoder table.*/
+int RLCA();
+int RLD();
+int RR_HLp();
+int RR_r();
+int RRA();
+int RRC_HLp();
+int RRC_r();
+int RRCA();
+int RRD();
 int SLA_HLp();
 int SLA_r();
 int SLL_HLp();
@@ -150,10 +146,54 @@ int SRA_HLp();
 int SRA_r();
 int SRL_HLp();
 int SRL_r();
-int SUB_HLp();
-int SUB_n();
-int SUB_r();
-int XOR_HLp();
-int XOR_n();
-int XOR_r();
+
+/* ------------------------------- */
+/* --- Bit set, reset and test --- */
+/* ------------------------------- */
+int BIT_b_HLp();
+int BIT_b_r();
+int RES_b_HLp();
+int RES_b_r();
+int SET_b_HLp();
+int SET_b_r();
+
+/* ------------ */
+/* --- Jump --- */
+/* ------------ */
+int DJNZ_d();
+int JP_cc_nn();
+int JP_HLp();
+int JP_nn();
+int JR_cc_d();
+int JR_d();
+
+/* ----------------------- */
+/* --- Call and return --- */
+/* ----------------------- */
+int CALL_cc_nn();
+int CALL_nn();
+int RET();
+int RET_cc();
+int RETI();
+int RETN();
+int RST_y();
+
+/* -------------------- */
+/* --- Input/Output --- */
+/* -------------------- */
+int IN_A_np();
+int IN_Cp();
+int IN_r_Cp();
+int IND();
+int INDR();
+int INI();
+int INIR();
+int OTDR();
+int OTIR();
+int OUT_Cp_0();
+int OUT_Cp_r();
+int OUT_np_A();
+int OUTD();
+int OUTI();
+
 #endif
