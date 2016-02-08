8f938aa35b7f40ebb2c0042f5eb7ba60c71e20f2
J.Luis <root@heavydeck.net>
2016-02-08 17:51:39 +0100

    Complete peripheral unit test

--- test/peripheral/test_peripheral.c
+++ test/peripheral/test_peripheral.c
@@ -18,6 +18,9 @@
 #include <io/fake_io.h>
 #include <peripheral/peripheral.h>
 
+#define VALUE_AB 0xAA
+#define VALUE_BM 0x55
+
 static char message[256];
 
 TEST_GROUP(grp_peripheral_write);
@@ -28,6 +31,7 @@ TEST_SETUP(grp_peripheral_write){
     *perdbg_reg_bm() = 0;
     *perdbg_reg_control() = 0;
     io_stat = 0x00;
+    z80_data = 0;
     z80_n_wr = 0;
     z80_n_rd = 1;
     z80_n_mreq = 1;
@@ -44,15 +48,121 @@ TEST_SETUP(grp_peripheral_read){
     *perdbg_reg_bm() = 0;
     *perdbg_reg_control() = 0;
     io_stat = 0x00;
+    z80_data = 0;
     z80_n_wr = 1;
     z80_n_rd = 0;
     z80_n_mreq = 1;
     z80_n_ioreq = 0;
     sprintf(message, "");
+
+    *perdbg_reg_ab() = VALUE_AB;
+    *perdbg_reg_bm() = VALUE_BM;
 }
 
 TEST_TEAR_DOWN(grp_peripheral_read){
 
+}
+
+// -------------------------------------------------------------------------
+// --- BM port. Port 0xDD (Mirrors on all even address from 0xC1 to 0xFF  --
+// -------------------------------------------------------------------------
+TEST(grp_peripheral_read, get_bm_valid){
+    for (int i = 0xC1; i <= 0xFF; i = i + 2){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_EQUAL_MESSAGE(VALUE_BM, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_bm_invalid){
+    for (int i = 0xC0; i <= 0xFF; i = i + 2){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_BM, z80_data, message);
+    }
+    for (int i = 0; i < 0xC0; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_BM, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_bm_disabled){
+    io_stat = io_stat | IO_PERIPHERIAL;
+    for (int i = 0; i <= 0xFF; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_BM, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_bm_mreq){
+    z80_n_mreq = 0;
+    z80_n_ioreq = 1;
+    for (int i = 0; i <= 0xFF; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_BM, z80_data, message);
+    }
+}
+
+// -------------------------------------------------------------------------
+// --- AB port. Port 0xDC (Mirrors on all even address from 0xC0 to 0xFD  --
+// -------------------------------------------------------------------------
+TEST(grp_peripheral_read, get_ab_valid){
+    for (int i = 0xC0; i <= 0xFD; i = i + 2){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_EQUAL_MESSAGE(VALUE_AB, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_ab_invalid){
+    for (int i = 0xC1; i <= 0xFE; i = i + 2){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_AB, z80_data, message);
+    }
+    for (int i = 0; i < 0xC0; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_AB, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_ab_disabled){
+    io_stat = io_stat | IO_PERIPHERIAL;
+    for (int i = 0; i <= 0xFF; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_AB, z80_data, message);
+    }
+}
+
+TEST(grp_peripheral_read, get_ab_mreq){
+    z80_n_mreq = 0;
+    z80_n_ioreq = 1;
+    for (int i = 0; i <= 0xFF; ++i){
+        sprintf(message, "Testing port 0x%02X", (uint8_t)i);
+        z80_address = (uint16_t)i;
+        z80_data = 0;
+        per_tick();
+        TEST_ASSERT_NOT_EQUAL_MESSAGE(VALUE_AB, z80_data, message);
+    }
 }
 
 // ---------------------------------------------------------------------------------
@@ -119,7 +229,15 @@ TEST_GROUP_RUNNER(grp_peripheral_write){
 }
 
 TEST_GROUP_RUNNER(grp_peripheral_read){
+    RUN_TEST_CASE(grp_peripheral_read, get_ab_valid);
+    RUN_TEST_CASE(grp_peripheral_read, get_ab_invalid);
+    RUN_TEST_CASE(grp_peripheral_read, get_ab_disabled);
+    RUN_TEST_CASE(grp_peripheral_read, get_ab_mreq);
 
+    RUN_TEST_CASE(grp_peripheral_read, get_bm_valid);
+    RUN_TEST_CASE(grp_peripheral_read, get_bm_invalid);
+    RUN_TEST_CASE(grp_peripheral_read, get_bm_disabled);
+    RUN_TEST_CASE(grp_peripheral_read, get_bm_mreq);
 }
 
 // ----------------------
