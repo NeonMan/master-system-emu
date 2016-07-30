6ee597f64427106a552d7f8b5b8bd03199d72db0
J.Luis <root@heavydeck.net>
2016-07-30 16:31:28 +0200

    Fixed IO library; Bootloader ROM uses 75% less hacks now! xD

--- test-roms/sms/io.c
+++ test-roms/sms/io.c
@@ -1,36 +1,39 @@
 #include "io.h"
 
-/** Lower 2 bits must be always cleared, use this mask for that.*/
-#define UNDEFINED_BIT_MASK 0xFC
+/** Copy of the IO register.*/
+uint8_t io_reg;
 
 /** Sets the IO chip register with the given value.*/
 void io_set(uint8_t r){
   __io_chip = r;
+  io_reg = r;
 }
 
 /** Gets the IO register value.*/
 uint8_t io_get(){
-  return __io_chip;
+  return io_reg;
 }
 
 /** Enables a feature.*/
 uint8_t io_enable(uint8_t f){
   uint8_t new_reg;
-  new_reg = __io_chip;
+  new_reg = io_reg;
   new_reg = new_reg & f;
   new_reg = new_reg & UNDEFINED_BIT_MASK;
   
 	__io_chip = new_reg;
+  io_reg = new_reg;
 	return new_reg;
 }
 
 /** Disables a feature.*/
 uint8_t io_disable(uint8_t f){
   uint8_t new_reg;
-  new_reg = __io_chip;
+  new_reg = io_reg;
   new_reg = new_reg | f;
   new_reg = new_reg & UNDEFINED_BIT_MASK;
   
 	__io_chip = new_reg;
-	return __io_chip;
+  io_reg = new_reg;
+	return new_reg;
 }
