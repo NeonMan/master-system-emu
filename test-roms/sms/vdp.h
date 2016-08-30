0b6fbc446ffdf68eacedf8abfc6bd9f0ff80098b
J.Luis <root@heavydeck.net>
2016-08-30 17:21:50 +0200

    Added skeleton VDP test ROM.

---
+++ test-roms/sms/vdp.h
@@ -0,0 +1,12 @@
+/**
+ * @file  vdp.h
+ * @brief Video chip functions.
+ */
+
+#ifndef __VDP_H
+#define __VDP_H
+
+#define VDP_DATA_PORT    0xBE
+#define VDP_ADDRESS_PORT 0xBF
+
+#endif
---
+++ test-roms/vdp-test.c
@@ -0,0 +1,16 @@
+/**
+ * @file  test-roms/vdp-test.c
+ * @brief Sample VDP message.
+ */
+
+#include "sms/vdp.h"
+#include "BBC_Micro_font.h"
+#include "sms/intv-dummy.h" /*<-- Ignore interrupts*/
+
+void main(){
+	
+	/*Do nothing*/
+	while(1){
+		
+	}
+}
