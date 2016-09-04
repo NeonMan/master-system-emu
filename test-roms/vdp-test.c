6168c8500cb103f8790aff547a7e606a98d960f9
J.Luis <root@heavydeck.net>
2016-09-04 18:03:18 +0200

    Initial console rom tests.

--- test-roms/vdp-test.c
+++ test-roms/vdp-test.c
@@ -3,18 +3,15 @@
  * @brief Sample VDP message.
  */
 
-#include "sms/vdp.h"
-#include "bbc_micro_font.h"
+#include "sms/console.h"
 #include "sms/intv-dummy.h" /*<-- Ignore interrupts*/
+#include <stdint.h>
 
 void main(){
-    volatile uint8_t c;
-    uint8_t i,j;
-    for(i=0; i<BBC_FONT_SIZE; i++){
-        for(j=0; j<BBC_FONT_CHAR_SIZE; j++){
-            c = bbc_font[i][j];
-        }
-    }
+    uint16_t i;
+    con_init();
+    
+    con_put("01234567890123456789012345678901");
     /*Do nothing*/
     while(1){
         
