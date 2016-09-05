c1e575f45fce347d6653c3819b7c54f323f06f31
J.Luis <root@heavydeck.net>
2016-09-05 22:34:34 +0200

    Moved main menu to an state.

--- test-roms/bios-menu/bios-menu.h
+++ test-roms/bios-menu/bios-menu.h
@@ -1,7 +1,22 @@
 #ifndef __BIOS_MENU_H
 #define __BIOS_MENU_H
 
+#define LEFT_MARGIN 2
+#define TOP_MARGIN 2
+#define MAX_OPTION_COUNT 20
+
+#define KEY_NONE 0
+#define KEY_UP   1
+#define KEY_DOWN 2
+#define KEY_LEFT 3
+#define KEY_RIGH 4
+#define KEY_1    5
+#define KEY_2    6
+
+void redraw_cursor();
+
 void bm_state_init();
 void bm_state_tick();
+
 
 #endif
