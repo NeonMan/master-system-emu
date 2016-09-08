077fb8faf6a330d4f75c6dfac35946f49b486631
J.Luis <root@heavydeck.net>
2016-09-08 23:00:21 +0200

    Relocating Crt0 now working, with sample.

--- test-roms/sms/crt0-sms-ram.s
+++ test-roms/sms/crt0-sms-ram.s
@@ -29,92 +29,106 @@
 ;--------------------------------------------------------------------------
 ;  Based off the SDCC z80 Crt0. 
 ;
-;  Modifications made so it can relocate into RAM and run from there
+;  Modifications made so it can relocate into RAM and run from there.
 ;--------------------------------------------------------------------------
 
 	.module crt0
 	.globl	_main
-
 	.area	_HEADER (ABS)
-    
-	;; Reset vector
-	.org 	0
-	di
-	jp	init
-
-	.org	0x08
-	call _rst_08h
-	reti
-
-	.org	0x10
-	call _rst_10h
-	reti
-
-	.org	0x18
-	call _rst_18h
-	reti
-
-	.org	0x20
-	call _rst_20h
-	reti
-
-	.org	0x28
-	call _rst_28h
-	reti
-
-	.org	0x30
-	call _rst_08h
-	reti
-
-	.org	0x38
-	call _rst_38h
-	reti
-
-nmi_hook:
-	;NMI call code does not fit it INT1 is being used, so we make an extra
-	;jump before performing the call
-	call _nmi
-	reti
 	
-	;NMI vector
-	.org	0x66
-	jr nmi_hook
-	
-	;INT vector
-	.org	0x68
-	call _int1
-	reti
-	
-	.org	0x50
-	.ascii "SMS RAM Crt 0.1"
+	.org	0x0000
 	.db 0x00
+	.ascii "Self-reloacating ROM. Trim all data before the 0xc000 offset"
 	
-	;.org	0x100
-	.org	0x70
+	;; Reset vector
+	.org 	0xc000
+entry:
+	;;No interrupts. we cannot use most of those anyway...
+	di
+	jr init
+	ENTRY_DIFFERENCE = ret_check - entry
+	RELOCATE_SIZE = relocate_end - relocate
+	RELOCATE_ADDRESS = 0xDFF0 - RELOCATE_SIZE
+	RELOCATE_OFFSET  = relocate - entry
+	RAM_BASE_ADDRESS = 0xc000
+	
 init:
-	;; Set stack pointer directly above top of memory.
-	ld	sp,#0xE000
-    ;; Set SEGA mapper to the default 0,1,2 slots
-    ld  hl,#0xFFFD
-    ld  (hl),#0x00
-    inc hl
-    ld  (hl),#0x01
-    inc hl
-    ld  (hl),#0x02
-
-	;; Initialise global variables
-gsinit::
-	ld	bc, #l__INITIALIZER
-	ld	a, b
-	or	a, c
-	jr	Z, gsinit_next
-	ld	de, #s__INITIALIZED
-	ld	hl, #s__INITIALIZER
+	;; Set stack pointer directly above top of memory minus one byte
+	ld	sp,#0xDFFF
+	
+	;; Set SEGA mapper to the default 0,1,2 slots
+	ld  hl,#0xFFFD
+	ld  (hl),#0x00
+	inc hl
+	ld  (hl),#0x01
+	inc hl
+	ld  (hl),#0x02
+	
+	;;write a 'RET' instruction (0xC9) at the 0xDFFF address
+	ld hl,#0xDFFF
+	ld (hl),#0xC9
+	
+	;;Call 0xDFFF, this way we have the _real_ address of ret_check
+	;;On the stack
+	call #0xDFFF
+ret_check:
+	;;Make SP point to the 'return' address, real address of ret_check
+	dec sp
+	dec sp
+	;;The real address of 'ret_check' is now ready to be 'popped out'.
+	;;We must eventually have the real address of 'entry' ready to be
+	;;used for an LDIR copy-relocation of the program. (HL reg)
+	
+	;;Calculate the difference between 'ret_check' and 'entry'
+	;;And push it back into the stack
+	pop hl
+	ld bc,#ENTRY_DIFFERENCE
+	scf
+	ccf
+	sbc hl,bc
+	push hl
+	
+	;;The real address of 'entry' is now at the stack top, now we need to
+	;;copy the whole program from there to the RAM base address (0xC000)
+	;;But first... the opcodes required to do this must be outside of the
+	;;copy path, so we write it just below the stack.
+	ld de,#RELOCATE_ADDRESS
+	  ;;Calculate real address of relocate
+	  pop hl
+	  dec sp ;;
+	  dec sp ;;Make SP point to the real entry address again
+	  scf
+	  ccf
+	  ld bc,#RELOCATE_OFFSET
+	  adc hl,bc
+	ld bc,#RELOCATE_SIZE
 	ldir
-gsinit_next:
+	;;Jump to relocation. We will never get back to the original ROM copy
+	jp RELOCATE_ADDRESS
+
+relocate:
+	;;Objective, copy 4K of data from the real address of 'entry' to the 
+	;;base address of RAM (0xC000)
+	ld de,#RAM_BASE_ADDRESS
+	pop hl ;;<-- Retrieve the real address of 'entry'
+	ld bc,#0x1000
+	ldir
+	;;Move SP back to the ram top
+	ld sp,#0xE000
+	;;By now all data should be on its correct place. So we call _main
 	call	_main
 	jp	_exit
-
+relocate_end:
+	
+	
+	
+	
+	
+	;;Signature to easily check the Crt being used.
+crt_signature:
+	.ascii "SMS RAM Crt0 V0.1"
+	.db 0x00
+	
 	;; Ordering of segments for the linker.
 	.area	_HOME
 	.area	_CODE
@@ -135,6 +149,6 @@ _exit::
 	ld	a,#0xFF
 1$:
 	.db 0xFD ;
-	halt     ; NONI/HALT used for emulator hooks.
+	halt	 ; NONI/HALT used for emulator hooks.
 	jr	1$
 
