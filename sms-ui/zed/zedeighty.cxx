32f2cbc25db49f016c87c562016c2c0a5d77b511
J.Luis <root@heavydeck.net>
2017-02-01 21:18:41 +0100

    Minor disassembler fixes.

--- sms-ui/zed/zedeighty.cxx
+++ sms-ui/zed/zedeighty.cxx
@@ -17,6 +17,9 @@ void zed::analyze(const uint8_t * rom, uint16_t start_addr, uint16_t end_addr)
     std::list<z80d_opcode> unsafe_jumps;
     std::list<z80d_opcode> normal_ops;
 
+    std::list<uint16_t> call_entry;
+    std::list<uint16_t> call_return;
+
     bool hit_address[256 * 256];
 
     for (int i = 0; i < (256 * 256); i++) {
@@ -53,12 +56,19 @@ void zed::analyze(const uint8_t * rom, uint16_t start_addr, uint16_t end_addr)
         if ((op.flags & Z80D_TYPE_NORMAL)) {
             frontier.push_back(op.address_next);
             normal_ops.push_back(op);
+            if (op.flags & Z80D_TYPE_RETURN) {
+                call_return.push_back(op.address);
+            }
         }
 
         //If opcode is "Constant jump" push into frontier and save
         if ((op.flags & Z80D_TYPE_CONST_JUMP)) {
             frontier.push_back(op.address_jump);
             jumps.push_back(op);
+
+            if (op.flags & Z80D_TYPE_CALL) {
+                call_entry.push_back(op.address_jump);
+            }
         }
 
         //If opcode is "Relative jump" push into frontier and save
@@ -70,6 +80,61 @@ void zed::analyze(const uint8_t * rom, uint16_t start_addr, uint16_t end_addr)
         //If opcode is "Indirect", save
         if ((op.flags & Z80D_TYPE_INDIRECT_JUMP)) {
             unsafe_jumps.push_back(op);
+            if (op.flags & Z80D_TYPE_RETURN) {
+                call_return.push_back(op.address);
+            }
         }
     }
+    //Sort the lists so we can easily discard repeats.
+    call_entry.sort();
+    call_return.sort();
+
+    //Get all unique call entries.
+    {
+        std::list<uint16_t> tmp_list;
+        uint16_t last_addr;
+
+        tmp_list = call_entry;
+        call_entry.clear();
+
+        last_addr = tmp_list.front();
+        tmp_list.pop_front();
+        call_entry.push_back(last_addr);
+
+        while (tmp_list.size() > 0) {
+            if (tmp_list.front() == last_addr) {
+                //Do nothing
+            }
+            else {
+                last_addr = tmp_list.front();
+                call_entry.push_back(last_addr);
+            }
+            tmp_list.pop_front();
+        }
+    }
+
+    //Get all unique call exits.
+    {
+        std::list<uint16_t> tmp_list;
+        uint16_t last_addr;
+
+        tmp_list = call_return;
+        call_return.clear();
+
+        last_addr = tmp_list.front();
+        tmp_list.pop_front();
+        call_return.push_back(last_addr);
+
+        while (tmp_list.size() > 0) {
+            if (tmp_list.front() == last_addr) {
+                //Do nothing
+            }
+            else {
+                last_addr = tmp_list.front();
+                call_return.push_back(last_addr);
+            }
+            tmp_list.pop_front();
+        }
+    }
+
 }
