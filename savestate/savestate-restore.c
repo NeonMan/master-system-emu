0467b62b04c4fc82afac8fdb47727f2293194374
J.Luis <root@heavydeck.net>
2017-04-28 04:36:29 +0200

    JSON restore parser initialization.

--- savestate/savestate-restore.c
+++ savestate/savestate-restore.c
@@ -17,7 +17,10 @@
  *  @brief Utilities to dump emulator state.
  */
 
+#include "b64.h"
+#include "jsmn.h"
 #include "savestate.h"
+#include <stdlib.h>
 #include <string.h>
 
 #include <ram/ram.h>
@@ -29,6 +32,30 @@
 #include <z80/z80_macros.h>
 
 int ss_restore(FILE* f){
+    //Read file, 8MB should be enough for everyone
+    uint8_t* sav_buffer = (uint8_t*) malloc((1024 * 1024 * 8) + 1);
     int rv = 0;
+    size_t size = fread(sav_buffer, 1, 1024 * 1024 * 8, f);
+    sav_buffer[size] = 0;
+
+    //Parse JSON tokens
+    jsmn_parser parser;
+    jsmntok_t tokens[100];
+    jsmn_init(&parser);
+
+    jsmn_parse(&parser, sav_buffer, strlen(sav_buffer), tokens, 100);
+
+    /*
+    dump_rom_mapper(f);
+    dump_io(f);
+    dump_peripheral(f);
+    dump_psg(f);
+    dump_z80(f);
+    dump_vdp(f);
+    dump_ram(f);
+    dump_vram(f);
+    */
+
+    free(sav_buffer);
     return rv;
 }
